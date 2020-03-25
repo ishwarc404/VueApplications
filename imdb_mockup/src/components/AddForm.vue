dcaa5ca86c2defc1c1247479274179793e9e8877
Ishwar Choudhary <ishwar@github.com>
2020-03-25 16:36:21 +0530

    added form capability

---
+++ imdb_mockup/src/components/AddForm.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <v-form ref="form" v-model="valid" lazy-validation class="formClass">
+      <v-text-field
+        v-model="name"
+        label="Movie name"
+        required
+        outlined
+      ></v-text-field>
+      <v-text-field
+        v-model="name"
+        label="Year"
+        required
+        outlined
+      ></v-text-field>
+      <v-text-field
+        v-model="name"
+        label="Plot"
+        required
+        outlined
+      ></v-text-field>
+      <v-text-field
+        v-model="name"
+        label="Poster URL"
+        required
+        outlined
+      ></v-text-field>
+
+      <v-btn color="success" class="mr-4">
+        Add Movie
+      </v-btn>
+
+      <v-btn color="error" class="mr-4" @click="reset">
+        Reset Form
+      </v-btn>
+    </v-form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.formClass {
+  width: 70%;
+  padding-left: 30%;
+}
+</style>
--- imdb_mockup/src/components/AddMovies.vue
+++ imdb_mockup/src/components/AddMovies.vue
@@ -1,16 +1,27 @@
 <template>
   <v-app>
     <NavBar />
+    <br />
+    <div class="d-flex justify-center">
+      <h1 class="top10text">Add Movies</h1>
+    </div>
+    <br>
+    
+    <AddForm/>
   </v-app>
 </template>
 
 <script>
 import NavBar from "./NavBar";
+import AddForm from "./AddForm";
 
 export default {
   name: "AddMovies",
   components: {
-    NavBar
+    NavBar,
+    AddForm
   }
 };
 </script>
+
+<style scoped></style>
