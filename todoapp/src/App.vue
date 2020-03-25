56c561dd355a9d554541bb5bfedd2a92f8f7acc4
Ishwar Choudhary <ishwar@github.com>
2020-03-25 09:47:00 +0530

    testing app

---
+++ todoapp/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <!-- <HelloWorld></HelloWorld> -->
+    <!-- msg is a prop -->
+    <Todos v-bind:todos="todos" />
+  </div>
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld.vue";
+// importing the HelloWorld component
+import Todos from "./components/Todos.vue";
+
+export default {
+  name: "App",
+  components: {
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo one",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Todo two",
+          completed: false
+        },
+        {
+          id: 3,
+          title: "Todo three",
+          completed: false
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Helvetica;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ todoapp/src/components/Todos.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>TO DOs</h1>
+    <div v-bind:key="todo.id" v-for="todo in todos">
+      <h3>{{todo.title}}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todos",
+  props: ["todos"],
+  components: {}
+};
+</script>
+
+
+<style lang="stylus" scoped></style>
